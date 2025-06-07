bb385fd2ef89edbadad235b2a27ae4d5552ba72f
Sibaik17 <okal_pratama_rahillah@students.usu.ac.id>
2025-06-07 19:18:20 +0800

    Dashboard BA Dismantle.py

--- Dashboard BA Dismantle.py
+++ Dashboard BA Dismantle.py
@@ -22,7 +22,7 @@ authenticator = stauth.Authenticate(
     cookie_expiry_days=1
 )
 
-name, authentication_status, username = authenticator.login("Login", "sidebar")
+name, authentication_status, username = authenticator.login("Login", "main")
 
 # ===============================
 # HANDLE LOGIN STATUS
