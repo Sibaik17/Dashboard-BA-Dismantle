0a1d556fb7b39d73029933a7cdab2eee5081d11e
Sibaik17 <okal_pratama_rahillah@students.usu.ac.id>
2025-06-07 20:10:50 +0800

    Dashboard BA Dismantle.py

--- Dashboard BA Dismantle.py
+++ Dashboard BA Dismantle.py
@@ -103,9 +103,9 @@ elif authentication_status:
                 st.pyplot(fig)
 
                 # Tabel detail Not Comply / Not Yet Assess
-                st.subheader("🔍 Detail Remarks (Not Comply / Not Yet Assess)")
+                st.subheader("🔍 Detail Remarks (Not Comply / NY Assess)")
                 filtered_df = df[df["Accuracy"].isin(["Not Comply", "NY Assess"])]
-                st.dataframe(filtered_df[["SiteID", "SiteName", "Accuracy", "Detail"]], use_container_width=True)
+                st.dataframe(filtered_df[["SONumb", "SiteName", "Accuracy", "Detail"]], use_container_width=True)
             else:
                 st.error("❗ Sheet 'DismantlePerangkat' tidak ditemukan.")
         except Exception as e:
